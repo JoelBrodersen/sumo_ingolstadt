3664ed1e888956a99d55eef7f2ea8823a3b3910e
JoelBrodersen <117082386+JoelBrodersen@users.noreply.github.com>
2023-03-08 17:58:36 +0100

    revised x,y search for offset defined bus_stop loc

--- bus_stop_location/GTFS/gtfs2csv_with_latlonxy.py
+++ bus_stop_location/GTFS/gtfs2csv_with_latlonxy.py
@@ -7,9 +7,12 @@ import pandas as pd
 
 #this script converts the gtfs_pt_stops_edit_10_24.add.xml into a csv file with the columns id, lat, lon, x, y (gtfs_stations.csv)
 
-def get_stops_data(): #Converts the XML-Dtaa in the additional file into a Pandas DataFrame, checks for faulty Stops
+
+
+
+def get_stops_data(): #Converts the XML-Data in the additional file into a Pandas DataFrame, checks for faulty Stops
     path_py = pathlib.Path(__file__)
-    path_stops = path_py.with_name("gtfs_with_dhid_copy.add.xml")
+    path_stops = path_py.parents[2].joinpath("simulation\gtfs_pt_stops_edit_10_24.add.xml") 
     tree_busstops = ET.parse(path_stops)
     additional = tree_busstops.getroot()
     id_list = []
@@ -33,16 +36,21 @@ def get_stops_data(): #Converts the XML-Dtaa in the additional file into a Panda
             start_pos_list.append("0")
         end_pos_list.append(busStop.attrib["endPos"])
     
-    
+
+
     df_busStops = pd.DataFrame({"id":id_list,"name":name_list,"lane":lane_list,"startPos":start_pos_list,"endPos":end_pos_list})
     return df_busStops,faulty_stops_list
 
+
+
 path_py = pathlib.Path(__file__)
-path_net = path_py.with_name('ingolstadt_24h-edit.net.xml')
-net = sumolib.net.readNet(path_net)
+path_net = path_py.parents[2].joinpath('simulation\ingolstadt_24h-edit.net.xml')
+
+net = sumolib.net.readNet(path_net,**{"withInternal":True})
+print(net)
 
 df_busStops,flist = get_stops_data()
-
+print(df_busStops)
 x_list = []
 y_list = []
 lat_list = []
@@ -50,7 +58,11 @@ lon_list = []
 id_list = []
 
 for index in range(0, len(df_busStops)):
-    x,y = sumolib.geomhelper.positionAtShapeOffset(net.getLane(df_busStops._get_value(index, 'lane')).getShape(), 0)
+    shape = net.getLane(df_busStops._get_value(index, 'lane')).getShape()
+    print(df_busStops._get_value(index, 'startPos'))
+    print(df_busStops._get_value(index, 'endPos'))
+    offset = (float(df_busStops._get_value(index, 'startPos')) + float(df_busStops._get_value(index, 'endPos')))/2 # mid value startPos and endPos
+    x,y = sumolib.geomhelper.positionAtShapeOffset(net.getLane(df_busStops._get_value(index, 'lane')).getShape(), offset)
     lon,lat = net.convertXY2LonLat(x, y)
     x_list.append(x)
     y_list.append(y)
@@ -58,8 +70,7 @@ for index in range(0, len(df_busStops)):
     lat_list.append(lat)
     id_list.append(df_busStops._get_value(index,'id'))
 
-
 #export collected data to csv file
 df_csv = pd.DataFrame({"id":id_list,"lat":lat_list,"lon":lon_list,"x":x_list,"y":y_list})
-df_csv.to_csv('gtfs_stations_copy.csv', index=False)
+df_csv.to_csv('gtfs_stations_small.csv', index=False)
     
